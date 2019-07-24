64ba2960fc4f936e472dfa51840d6538e27d160e
Tyler Thompson <th0mps0n.ty@protonmail.com>
2019-07-23 22:27:37 -0400

    added images for menu and css transitions

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -2,186 +2,181 @@ export const foodItems = [
   // Proteins
   {
     name: 'Cod',
-    img: '/img/slide_two.jpg',
+    img: '/img/cod.jpeg',
     section: 'Protein'
   },
   {
     name: 'Salmon',
-    img: '/img/slide_three.jpg',
+    img: '/img/salmon.jpg',
     section: 'Protein'
   },
   {
     name: 'Steak',
-    img: '/img/slide_three.jpg',
+    img: '/img/steak.jpg',
     section: 'Protein'
   },
   {
     name: 'Chicken Breast',
-    img: '/img/slide_three.jpg',
+    img: '/img/chicken-breast.jpg',
     section: 'Protein'
   },
   {
     name: 'Chicken Sausage',
-    img: '/img/slide_three.jpg',
+    img: '/img/chicken-sausage.jpg',
     section: 'Protein'
   },
   {
     name: 'Turkey Breast',
-    img: '/img/slide_three.jpg',
+    img: '/img/turkey-breast.jpg',
     section: 'Protein'
   },
   {
     name: 'Turkey Patty',
-    img: '/img/slide_three.jpg',
+    img: '/img/turkey-patty.jpg',
     section: 'Protein'
   },
   {
     name: 'Turkey Tenderloin',
-    img: '/img/slide_three.jpg',
+    img: '/img/turkey-tenderloin.jpg',
     section: 'Protein'
   },
   {
     name: 'Turkey Sausage',
-    img: '/img/slide_three.jpg',
+    img: '/img/turkey-sausage.jpg',
     section: 'Protein'
   },
   // Flavors
   {
     name: 'Plain',
-    img: '/img/slide_three.jpg',
+    img: '/img/plain.jpg',
     section: 'Flavor'
   },
   {
     name: 'Garlic',
-    img: '/img/slide_three.jpg',
+    img: '/img/garlic.jpg',
     section: 'Flavor'
   },
   {
     name: 'Extra Spicy',
-    img: '/img/slide_three.jpg',
+    img: '/img/extra-spicy.jpg',
     section: 'Flavor'
   },
   {
     name: 'Garlic & Pepper',
-    img: '/img/slide_three.jpg',
+    img: '/img/garlic-and-pepper.jpg',
     section: 'Flavor'
   },
   {
     name: 'Southwest Chipotle',
-    img: '/img/slide_three.jpg',
+    img: '/img/southwest-chipotle.jpg',
     section: 'Flavor'
   },
   {
     name: 'Tomato Basil Garlic',
-    img: '/img/slide_three.jpg',
+    img: '/img/tomato-basil-garlic.jpg',
     section: 'Flavor'
   },
   {
     name: 'Lemon Pepper',
-    img: '/img/slide_three.jpg',
+    img: '/img/lemon-pepper.jpg',
     section: 'Flavor'
   },
   // Veggies
   {
     name: 'Broccoli',
-    img: '/img/slide_three.jpg',
+    img: '/img/broccoli.jpg',
     section: 'Vegetable'
   },
   {
     name: 'Asparagus',
-    img: '/img/slide_three.jpg',
+    img: '/img/asparagus.jpg',
     section: 'Vegetable'
   },
   {
     name: 'Spinach',
-    img: '/img/slide_three.jpg',
+    img: '/img/spinach.jpg',
     section: 'Vegetable'
   },
   {
     name: 'Carrots',
-    img: '/img/slide_three.jpg',
+    img: '/img/carrots.jpg',
     section: 'Vegetable'
   },
   {
     name: 'Kale',
-    img: '/img/slide_three.jpg',
+    img: '/img/kale.jpg',
     section: 'Vegetable'
   },
   {
     name: 'Brussel Sprouts',
-    img: '/img/slide_three.jpg',
+    img: '/img/brussel-sprouts.jpg',
     section: 'Vegetable'
   },
   {
     name: 'Cauliflower',
-    img: '/img/slide_three.jpg',
+    img: '/img/cauliflower.jpg',
     section: 'Vegetable'
   },
   {
     name: 'Collard Greens',
-    img: '/img/slide_three.jpg',
+    img: '/img/collard-greens.jpg',
     section: 'Vegetable'
   },
   {
     name: 'Bell Peppers',
-    img: '/img/slide_three.jpg',
+    img: '/img/bell-peppers.jpg',
     section: 'Vegetable'
   },
   {
     name: 'Green Beans',
-    img: '/img/slide_three.jpg',
+    img: '/img/green-beans.jpg',
     section: 'Vegetable'
   },
   {
     name: 'Zucchini / Squash',
-    img: '/img/slide_three.jpg',
+    img: '/img/zucchini.jpg',
     section: 'Vegetable'
   },
   // Sides
   {
     name: 'Brown Rice',
-    img: '/img/slide_three.jpg',
-    section: 'Sides'
+    img: '/img/brown-rice.jpg',
+    section: 'Side'
   },
   {
     name: 'Quinoa',
-    img: '/img/slide_three.jpg',
-    section: 'Sides'
+    img: '/img/quinoa.jpg',
+    section: 'Side'
   },
   {
     name: 'Jasmine Rice',
-    img: '/img/slide_three.jpg',
-    section: 'Sides'
+    img: '/img/jasmine-rice.jpg',
+    section: 'Side'
   },
   {
     name: 'Fresh Fruit',
-    img: '/img/slide_three.jpg',
-    section: 'Sides'
+    img: '/img/fresh-fruit.jpg',
+    section: 'Side'
   },
   {
     name: 'Sweet Potatoes',
-    img: '/img/slide_three.jpg',
-    section: 'Sides'
+    img: '/img/sweet-potatoes.jpg',
+    section: 'Side'
   },
   {
     name: 'Black Beans & Tomatoes',
-    img: '/img/slide_three.jpg',
-    section: 'Sides'
+    img: '/img/black-beans-tomatoes.jpg',
+    section: 'Side'
   },
   {
     name: 'Chickpeas & Kidney',
-    img: '/img/slide_three.jpg',
-    section: 'Sides'
-  },
-  {
-    name: 'Beans',
-    img: '/img/slide_three.jpg',
-    section: 'Sides'
+    img: '/img/chickpeas-kidney.jpg',
+    section: 'Side'
   },
   {
     name: 'Roasted Corn',
-    img: '/img/slide_three.jpg',
-    section: 'Sides'
+    img: '/img/roasted-corn.jpg',
+    section: 'Side'
   }
 ];
 
@@ -192,5 +187,3 @@ export const foods = foodItems.reduce((res, food) => {
   res[food.section].push(food);
   return res;
 }, {});
-// ToDO: List all individual
-// ingredients and d/l images for each
--- src/Menu/FoodGrid.js
+++ src/Menu/FoodGrid.js
@@ -20,11 +20,17 @@ export const Food = styled(Title)`
   background-image: ${({ img }) => `url(${img});`};
   background-position: center;
   background-size: cover;
+  margin-top: 5px;
   filter: contrast(75%);
   border-radius: 7px;
-  box-shadow: 0px 0px 10px 0px grey;
+  transition-property: box-shadow margin-top filter;
+  transition-duration: 0.1s;
+  box-shadow: 0px 0px 2px 0px grey;
   &:hover {
     cursor: pointer;
-    opacity: 0.7;
+    margin-top: 0px;
+    margin-bottom: 5px;
+    filter: contrast(100%);
+    box-shadow: 0px 5px 12px 0px grey;
   }
 `;
