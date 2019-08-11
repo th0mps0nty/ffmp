c612b9f592862a40648305ab8d8136b2b69bf534
Tyler Thompson <th0mps0n.ty@protonmail.com>
2019-08-10 20:24:11 -0400

    added Total, Sub-Total, and Tax

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -50,6 +50,12 @@ const OrderItem = styled.div`
 `;
 
 export function Order({ orders }) {
+  const subtotal = orders.reduce((total, order) => {
+    return total + getPrice(order);
+  }, 0);
+  const tax = subtotal * 0.07;
+  const total = subtotal + tax;
+
   return (
     <OrderStyled>
       {orders.length === 0 ? (
@@ -69,6 +75,23 @@ export function Order({ orders }) {
               </OrderItem>
             </OrderContainer>
           ))}
+          <OrderContainer>
+            <OrderItem>
+              <div />
+              <div>Sub-Total</div>
+              <div>{formatPrice(subtotal)}</div>
+            </OrderItem>
+            <OrderItem>
+              <div />
+              <div>Tax</div>
+              <div>{formatPrice(tax)}</div>
+            </OrderItem>
+            <OrderItem>
+              <div />
+              <div>Total</div>
+              <div>{formatPrice(total)}</div>
+            </OrderItem>
+          </OrderContainer>
         </OrderContent>
       )}
       <DialogFooter>
